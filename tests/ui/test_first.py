3c61a08c7a5774d7ee773ef877b495eaf0b277b0
butsko_as <butsko_as@astralnalog.ru>
2024-09-06 14:44:48 +0300

    ci - target6

--- tests/ui/test_first.py
+++ tests/ui/test_first.py
@@ -16,6 +16,7 @@ driver.maximize_window()
 # Задаем список доступных юзеров и пароль
 list_of_users = ['standard_user', 'locked_out_user', 'problem_user', 'performance_glitch_user', 'error_user', 'visual_user']
 password = 'secret_sauce'
+
 def test_first():
 
     # Прогоняем через цикл
